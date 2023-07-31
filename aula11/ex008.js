e8da0e1b8b3762271e51f6dfc7aea6887568d137
BrunaSolemar <136350364+BrunaSolemar@users.noreply.github.com>
2023-07-31 10:00:34 -0300

    condição composta

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,6 @@
 var vel = 60.5
 console.log(`A velocidade do seu carro é ${vel}km/h`)
-
+if (vel > 60) {
+    (`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
 console.log(`Dirija sempre usando cinto de segurança`)
