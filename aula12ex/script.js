fc8d8a9aa5f6b4677f7594dfa17bcc3e5832cb41
Bruna Solemar <brunasolemar@gmail.com>
2023-09-09 00:24:14 -0300

    .

--- aula12ex/script.js
+++ aula12ex/script.js
@@ -1,1 +1,18 @@
-alert(`Olá!`)
+function carregar(){
+  var msg = window.document.getElementById(`msg`)
+  var img = window.document.getElementById(`imagem`) 
+  var data = new Date()
+  var hora = data.getHours()
+  msg.innerHTML = `Agora são ${hora} horas.`
+  if (hora >= 0 && < 12) {
+    //Bom dia
+    img.src = `ex14/manha.png`
+  } else if (hora >= 12 && hora < 18){
+    //Boa tarde
+    img.src = `ex14/tarde.png`
+  } else {
+    //Boa noite
+    img.src = `ex14/noite.png`
+  }
+}
+
