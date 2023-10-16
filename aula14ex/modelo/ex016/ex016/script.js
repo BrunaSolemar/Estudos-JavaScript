30030e4d27c1d62d49497349d71c88b7fce1052c
BrunaSolemar <136350364+BrunaSolemar@users.noreply.github.com>
2023-10-16 13:04:06 -0300

    .

--- aula14ex/modelo/ex016/ex016/script.js
+++ aula14ex/modelo/ex016/ex016/script.js
@@ -1,26 +1,14 @@
-function carregar(){
-  var msg = window.document.getElementById(`msg`)
-  var img = window.document.getElementById(`imagem`) 
-  var data = new Date()
-  var hora = data.getHours() //hora atual do sistema
-  
-  msg.innerHTML = `Agora são ${hora} horas.`
-  if (hora >= 6 && hora < 12) {
-    //Bom dia
-    img.src = `ex14/manha.png`
-    document.body.style.background = `#d6a57d` //está deixando o corpo do site com o mesmo tom da foto que aparece referente ao horário
-  } else if (hora >= 12 && hora < 18){
-    //Boa tarde
-    img.src = `ex14/tarde.png`
-    document.body.style.background = `#737b86`
-  } else if (hora >= 18 && hora > 0) {
-    //Boa noite
-    img.src = `ex14/noite.png`
-    document.body.style.background = `#1d55a7`
-  } else {
-    //Boa madrugada
-    img.src = `ex14/madrugada.png`
-    document.body.style.background = `#001531`
-  }
-}
+function contar() {
+  var ini = document.getElementById('txti')
+  var fim = document.getElementById('txtf')
+  var passo = document.getElementById ('txtp')
+  var res = document;getElementById('res')
 
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('[ERRO] Faltam dados!')
+ 
+  }else {
+    res.innerHTML = 'Contando: '
+  } 
+
+}
