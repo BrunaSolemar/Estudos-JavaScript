7f7f4319e4f2983d8921ae5db868f41411cfaba9
BrunaSolemar <136350364+BrunaSolemar@users.noreply.github.com>
2023-10-16 09:00:11 -0300

    ex14

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+/*repetição com teste no fim*/ 
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+/* teste no inicio
+var c = 1 //contador a partir do número 1
+while (c <= 6) { //enquanto o contador for menor ou igual a 6 (esse número pode ser ajustado)
+    console.log('Tudo bem?') //vai aparecer a frase 'Tudo bem?'
+    c++ //c = c + 1 - enquanto c menor ou igual a 6 vai repetir a frase
+}*/
